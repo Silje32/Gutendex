c4de754aa7428dcf0962914b350561f2b52ab72e
Silje32 <msh070425@gmail.com>
2025-09-24 13:24:30 +0200

    Added my home path

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,13 @@ import "./index.css";
 // Router components
 import App from "./App.jsx";
 
-const router = createBrowserRouter();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <p>Error</p>,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
