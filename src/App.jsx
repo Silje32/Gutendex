8ca2f6922b8daf3cec8957ec2c871d9023cd11cf
Silje32 <msh070425@gmail.com>
2025-09-24 14:49:14 +0200

    Imported the outlet and added the outlet to my function App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
+      <Outlet />
       <Footer />
     </>
   );
