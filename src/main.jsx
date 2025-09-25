c98bfb3f17f5eea390bf4d77ab1249b70621be03
Silje32 <msh070425@gmail.com>
2025-09-25 14:59:32 +0200

    Imported the FavoritesPage and added a favorites route

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import App from "./App.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import DetailsPage from "./pages/DetailsPage.jsx";
 import CategoryPage from "./pages/CategoryPage.jsx";
+import FavoritesPage from "./pages/FavoritesPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: "/favorites",
+        element: <FavoritesPage />,
       },
       {
         path: "/details/:detailsId",
