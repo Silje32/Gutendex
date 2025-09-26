c50f5fd8741efee32c68ecc78a7ae825b5b9c58e
Silje32 <msh070425@gmail.com>
2025-09-26 13:59:31 +0200

    Imported the useEffect hook, created the useEffect hook and added my async API fetch call

--- src/components/FetchBooks.jsx
+++ src/components/FetchBooks.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function FetchBooks(props) {
   const [books, setBooks] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch("https://gutendex.com/books");
+        if (!response.ok) {
+          throw new Error(`HTTP error.status ${response.status}`);
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   function handleBooks(e) {
     e.preventDefault();
@@ -11,15 +31,19 @@ export default function FetchBooks(props) {
 
   return (
     <div>
-      <form onSubmit={handleBooks}>
-        <input
-          type="text"
-          placeholder="Search Books..."
-          onChange={(e) => setBooks(e.target.value)}
-        ></input>
-        <button type="submit">Search Icon</button>
-        <button>Favorites Icon</button>
-      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+      {books && (
+        <form onSubmit={handleBooks}>
+          <input
+            type="text"
+            placeholder="Search Books..."
+            onChange={(e) => setBooks(e.target.value)}
+          ></input>
+          <button type="submit">Search Icon</button>
+          <button>Favorites Icon</button>
+        </form>
+      )}
     </div>
   );
 }
