2fac1e07a5308f9fcea6b682bcf6481b7d85a1d2
Silje32 <msh070425@gmail.com>
2025-09-26 13:26:30 +0200

    Added my books, loading and error useState variables and moved my input, submit button and favorite button to my FetchBooks function

--- src/components/FetchBooks.jsx
+++ src/components/FetchBooks.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
 
-export default function FetchBooks() {
+export default function FetchBooks(props) {
+  const [books, setBooks] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  function handleBooks(e) {
+    e.preventDefault();
+  }
+
   return (
     <div>
-      <h1>Find Books</h1>
+      <form onSubmit={handleBooks}>
+        <input
+          type="text"
+          placeholder="Search Books..."
+          onChange={(e) => setBooks(e.target.value)}
+        ></input>
+        <button type="submit">Search Icon</button>
+        <button>Favorites Icon</button>
+      </form>
     </div>
   );
 }
