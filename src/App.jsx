ea8209938e0009507d262dc59d18ed760e845512
Silje32 <msh070425@gmail.com>
2025-09-30 18:40:35 +0200

    Imported useState, added my list and setList variables, added my searchBooks function and put my searchBooks prop on my outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+
+  function searchBooks(search) {
+    setList((prevBooks) => [...prevBooks, search]);
+  }
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet data={{ searchBooks }} />
       <Footer />
     </>
   );
