4a130b63a6f66467c3c73a9baf8159f8c72d127e
Silje32 <msh070425@gmail.com>
2025-09-30 14:50:21 +0200

    Removed useState and my SearchBooks function from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,8 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import { useState } from "react";
 
 function App() {
-  const [list, setList] = useState([]);
-
-  function searchBooks(search) {
-    setList((prevBooks) => [...prevBooks, search]);
-  }
   return (
     <>
       <Header />
