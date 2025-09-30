1163b7836733574d1d911c743e000f2173681530
Silje32 <msh070425@gmail.com>
2025-09-30 18:55:17 +0200

    Added my searchBooks prop to my Header function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import FetchBooks from "../components/FetchBooks.jsx";
 import { useState } from "react";
 
-export default function Header() {
+export default function Header({ data: { searchBooks } }) {
   const [list, setList] = useState([]);
   return (
     <header>
