bfbd4482a7c20a88573c6756bce236284b22367b
Silje32 <msh070425@gmail.com>
2025-10-15 14:55:03 +0200

    Fixed my exports

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+// Export all functions from individual modules
+export { default as HeaderList } from "./HeaderList.jsx";
+export { default as Header } from "./Header.jsx";
+export { default as Footer } from "./Footer.jsx";
+export { default as BookList } from "./BookList.jsx";
+export { default as BookItem } from "./BookItem.jsx";
+export { default as FetchBooks } from "./FetchBooks.jsx";
