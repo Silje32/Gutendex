281e28d8c3d2d6f73bd2d1fe9a154389dd86903a
Silje32 <msh070425@gmail.com>
2025-10-15 15:09:45 +0200

    Added my CategoryArray to my HeaderList

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,15 @@
 import { Link } from "react-router-dom";
-import FetchBooks from "../components/FetchBooks.jsx";
+import { FetchBooks, HeaderList } from "../components/index.jsx";
+import { CategoryArray } from "../data/CategoryArray.jsx";
 
-export default function Header({ data: { searchBooks } }) {
+export default function Header() {
   return (
     <header>
-      <nav>
-        <Link to="/category/:categoryName">Fiction</Link>
-        <Link to="/category/:categoryName">Mystery</Link>
-        <Link to="/category/:categoryName">Thriller</Link>
-        <Link to="/category/:categoryName">Romance</Link>
-        <Link to="/category/:categoryName">Fantasy</Link>
-        <Link to="/category/:categoryName">Morality</Link>
-        <Link to="/category/:categoryName">Society</Link>
-        <Link to="/category/:categoryName">Power</Link>
-        <Link to="/category/:categoryName">Justice</Link>
-        <Link to="/category/:categoryName">Adventure</Link>
-        <Link to="/category/:categoryName">Tragedy</Link>
-        <Link to="/category/:categoryName">War</Link>
-        <Link to="/category/:categoryName">Philosophy</Link>
-      </nav>
-
       <div>
         <Link to="/">GUTENDEX</Link>
       </div>
       <div>
+        <HeaderList categories={CategoryArray} />
         <FetchBooks />
       </div>
     </header>
