8c04fc245caca316f751f8a0b5fa8fbe4736ad2c
Silje32 <msh070425@gmail.com>
2026-01-20 12:30:43 +0100

    Added a lot of exports

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,7 @@
 // Export all functions from individual modules
 export { default as HeaderList } from "./HeaderList.jsx";
-export { default as Header } from "./Header.jsx";
-export { default as Footer } from "./Footer.jsx";
+export { default as Header } from "../components/header/Header.jsx";
+export { default as Footer } from "../components/footer/Footer.jsx";
 export { default as BookList } from "./BookList.jsx";
 export { default as BookItem } from "./BookItem.jsx";
 export { default as FetchBooks } from "./FetchBooks.jsx";
