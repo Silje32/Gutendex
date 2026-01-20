8702e5fe7152b6a1ba9d7aa7e25f96c0322fc4e5
Silje32 <msh070425@gmail.com>
2026-01-20 12:36:02 +0100

    Fixed my imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 import { useState } from "react";
 
 function App() {
