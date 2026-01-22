17874b6115434c171f86efd1d5beac6f7d2ad4d8
Silje32 <msh070425@gmail.com>
2026-01-22 14:43:52 +0100

    Styled my h1 heading

--- src/components/favourites/Favorites.jsx
+++ src/components/favourites/Favorites.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import styles from "../favourites/Favorites.module.css";
 
 export default function Favorites() {
   return (
-    <div>
-      <h1>Favorites</h1>
-    </div>
+    <>
+      <div>
+        <h1 className={styles.myfavorites}>MY FAVORITE BOOKS</h1>
+      </div>
+    </>
   );
 }
