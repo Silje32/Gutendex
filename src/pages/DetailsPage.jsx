c1cb13ab585cc770491225426db613fc6bb3ebee
Silje32 <msh070425@gmail.com>
2026-01-22 13:44:15 +0100

    Added my Details function to my DetailsPage

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,12 +1,7 @@
-import { useParams } from "react-router-dom";
-
 export default function Details() {
-  const { detailsId } = useParams();
-  console.log(detailsId);
-
   return (
-    <div>
-      <h1>Details {detailsId}</h1>
-    </div>
+    <>
+      <Details />
+    </>
   );
 }
