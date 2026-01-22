0e1bbbb4fd4cc9b0d656204b2e89a528d4f0f6e6
Silje32 <msh070425@gmail.com>
2026-01-22 12:47:24 +0100

    Added the BookList function to my homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,9 @@
 import BookList from "../components/home/BookList.jsx";
 
 export default function HomePage() {
-  return <ul>Booklist</ul>;
+  return (
+    <div>
+      <BookList />
+    </div>
+  );
 }
