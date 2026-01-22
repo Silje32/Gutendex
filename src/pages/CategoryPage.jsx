36e59e324a53dd2746affa0da656c5bbdfc24845
Silje32 <msh070425@gmail.com>
2026-01-22 14:02:44 +0100

    Updated my h1

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router-dom";
 
 export default function Category() {
-  const { categoryName } = useParams();
-  console.log(categoryName);
+  const { CategoryName } = useParams();
+  console.log(CategoryName);
 
   return (
     <div>
-      <h1>Category {categoryName}</h1>
+      <h1>{CategoryName}</h1>
     </div>
   );
 }
