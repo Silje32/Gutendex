c106793b02a49248ea4613b470b2d167185a90c8
Silje32 <msh070425@gmail.com>
2026-01-22 13:59:41 +0100

    Removed my BookList function and my BookItem function on my HomePage so the homepage can work again

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,6 @@
 import BookList from "../components/home/BookList.jsx";
+import { BookItem } from "../components/index.jsx";
 
 export default function HomePage() {
-  return (
-    <div>
-      <BookList />
-    </div>
-  );
+  return <></>;
 }
