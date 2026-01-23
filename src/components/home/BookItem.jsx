037e92ad874dfe38489d2dd08d89858589ac0c65
Silje32 <msh070425@gmail.com>
2026-01-23 14:36:57 +0100

    Updated my BookItem function

--- src/components/home/BookItem.jsx
+++ src/components/home/BookItem.jsx
@@ -1,7 +1,13 @@
 export default function BookItem({ data: { books } }) {
   return (
     <li>
-      <p className={styles.booklist}>{books.name}</p>
+      <p className={styles.booklist}>{styles.title}</p>
+      <h1>
+        {books.title} className={styles.booklist}
+      </h1>
+      <h2>{books.authors}</h2>
+      <img src={books.formats["image/jpeg"]} alt={books.title} />
+      <p>{books.summaries}</p>
     </li>
   );
 }
