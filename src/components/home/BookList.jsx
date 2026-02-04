09cd2c71e21437630d48ac9ecf6cddaa697669a8
Silje32 <msh070425@gmail.com>
2026-02-04 12:28:25 +0100

    Updated my BookList function

--- src/components/home/BookList.jsx
+++ src/components/home/BookList.jsx
@@ -1,10 +1,13 @@
 import BookItem from "./BookItem.jsx";
+import styles from "./BookList.module.css";
 
-export default function BookList({ data: { books } }) {
+export default function BookList({ books }) {
+  if (!books) return <p>Loading books...</p>;
+
   return (
     <ul>
       {books.map((book) => {
-        return <BookItem className={styles.booklist} key={book.id} />;
+        return <BookItem className={styles.booklist} key={book.id} {...book} />;
       })}
     </ul>
   );
