203fcdc450c7371cea5745d5c24e01f601a4a7e9
Silje32 <msh070425@gmail.com>
2026-02-04 14:00:29 +0100

    Updated my BookList function

--- src/components/home/BookList.jsx
+++ src/components/home/BookList.jsx
@@ -2,7 +2,7 @@ import BookItem from "./BookItem.jsx";
 import styles from "./BookList.module.css";
 
 export default function BookList({ books }) {
-  if (!books) return <p>Loading books...</p>;
+  if (!books) return <h1>Venter...</h1>;
 
   return (
     <ul>
