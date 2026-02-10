f3e912e59c0e2626d1543696e1f74dd054561a64
Silje32 <msh070425@gmail.com>
2026-02-10 12:31:49 +0100

    Added favorites button to my BookItem list

--- src/components/home/BookItem.jsx
+++ src/components/home/BookItem.jsx
@@ -1,15 +1,21 @@
+import FavoritesButton from "../buttons/Favoritesbutton";
 import styles from "./BookList.module.css";
 
 export default function BookItem({ title, authors, formats, summaries }) {
   return (
-    <li>
-      <p className={styles.booklist}>{title}</p>
-      <h1>{title}</h1>
-      {authors.map((author) => (
-        <h2>{author.name}</h2>
-      ))}
-      <img src={formats["image/jpeg"]} alt={title} />
-      <p>{summaries}</p>
-    </li>
+    <>
+      <div>
+        <FavoritesButton />
+      </div>
+      <li>
+        <p className={styles.booklist}>{title}</p>
+        <h1>{title}</h1>
+        {authors.map((author) => (
+          <h2>{author.name}</h2>
+        ))}
+        <img src={formats["image/jpeg"]} alt={title} />
+        <p>{summaries}</p>
+      </li>
+    </>
   );
 }
