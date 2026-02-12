29035323c2d3ece4410891db5acf8159fec1b8af
Silje32 <msh070425@gmail.com>
2026-02-12 11:50:57 +0100

    Added local storage

--- src/components/favourites/Favorites.jsx
+++ src/components/favourites/Favorites.jsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../favourites/Favorites.module.css";
 
 export default function Favorites() {
+  // Lagre favoritter i localStorage
+  useEffect(() => {
+    const storedFavorites = localStorage.setItem(
+      "storedFavorites",
+      JSON.stringify(storedFavorites),
+    );
+    if (storedFavorites) {
+      setStoredFavorites(JSON.parse(storedFavorites));
+    }
+  }, []);
+
+  // Hente favoritter fra localStorage
+  const [storedFavorites, setStoredFavorites] = useState(() => {
+    const storedFavorites = localStorage.getItem("storedFavorites");
+    return storedFavorites ? JSON.parse(storedFavorites) : [];
+  });
+
   return (
     <>
       <div>
