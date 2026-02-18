b56ebacec032af36f7fdb8c542d6d68d7747ba2b
Silje32 <msh070425@gmail.com>
2026-02-18 14:21:46 +0100

    Added Title, Author and Summary text

--- src/components/home/BookItem.jsx
+++ src/components/home/BookItem.jsx
@@ -6,16 +6,18 @@ export default function BookItem({ id, title, authors, formats, summaries }) {
     <>
       <div className={styles.container_home}>
         <li className={styles.booklist} key={id}>
-          <h1>{title}</h1>
+          <h1>Title: {title}</h1>
           {authors.map((author) => (
-            <h2>{author.name}</h2>
+            <h2>Author: {author.name}</h2>
           ))}
           <img
             src={formats["image/jpeg"]}
             alt={title}
             className={styles.images}
           />
-          <p>{summaries}</p>
+          <p>
+            <strong>Summary:</strong> {summaries}
+          </p>
         </li>
         <FavoritesButton />
         <div className={styles.hr}></div>
