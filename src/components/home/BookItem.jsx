0f868d7a1dbaaf109a100a8bf88da0749e51ac9d
Silje32 <msh070425@gmail.com>
2026-02-18 14:07:06 +0100

    Added id key to my BookItem function and added a div at the bottom of my function to make som space between the books

--- src/components/home/BookItem.jsx
+++ src/components/home/BookItem.jsx
@@ -1,12 +1,12 @@
 import FavoritesButton from "../buttons/Favoritesbutton";
 import styles from "./BookList.module.css";
 
-export default function BookItem({ title, authors, formats, summaries }) {
+export default function BookItem({ id, title, authors, formats, summaries }) {
   return (
     <>
       <div className={styles.container_home}>
-        <li className={styles.booklist}>
-          <h1 className={styles.h1}>{title}</h1>
+        <li className={styles.booklist} key={id}>
+          <h1>{title}</h1>
           {authors.map((author) => (
             <h2>{author.name}</h2>
           ))}
@@ -18,6 +18,7 @@ export default function BookItem({ title, authors, formats, summaries }) {
           <p>{summaries}</p>
         </li>
         <FavoritesButton />
+        <div className={styles.hr}></div>
       </div>
     </>
   );
