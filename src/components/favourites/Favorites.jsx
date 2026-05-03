85e8a3fd23a423e0a0ef3745042d3b5a6ae8450a
Silje32 <msh070425@gmail.com>
2026-05-03 11:19:36 +0200

    Moved my useState out of my useEffect

--- src/components/favourites/Favorites.jsx
+++ src/components/favourites/Favorites.jsx
@@ -4,21 +4,22 @@ import styles from "../favourites/Favorites.module.css";
 export default function Favorites() {
   // Lagre favoritter i localStorage
 
+  const [storedFavorites, setStoredFavorites] = useState([]);
   useEffect(() => {
-    localStorage.setItem("storedFavorites", JSON.stringify(storedFavorites));
+    const savedData = localStorage.getItem("storedFavorites");
+
+    if (!savedData) {
+      localStorage.setItem("storedFavorites", JSON.stringify(storedFavorites));
+    }
   }, [storedFavorites]);
 
   // Hente favoritter fra localStorage
-  const [storedFavorites, setStoredFavorites] = useState(() => {
+  useEffect(() => {
     const savedData = localStorage.getItem("storedFavorites");
-    return savedData ? JSON.parse(savedData) : [];
-  });
-
+  }, []);
   return (
-    <>
-      <div>
-        <h1 className={styles.myfavorites}>MY FAVORITE BOOKS</h1>
-      </div>
-    </>
+    <div>
+      <h1 className={styles.myfavorites}>MY FAVORITE BOOKS</h1>
+    </div>
   );
 }
